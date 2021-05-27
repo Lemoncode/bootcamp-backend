500e32078a41e15b1cea8be9060b75e01a256fb5
daniel.sanchez <daniel.sanchez@lemoncode.net>
2021-05-27 13:20:59 +0200

    implement example 01-core

---
+++ 00-stack-documental/03-rest-api/04-project-structure/01-core/src/core/constants/env.constants.ts
@@ -0,0 +1,6 @@
+export const envConstants = {
+  PORT: process.env.PORT,
+  STATIC_FILES_PATH: process.env.STATIC_FILES_PATH, 
+  CORS_ORIGIN: process.env.CORS_ORIGIN,
+  CORS_METHODS: process.env.CORS_METHODS,
+};
