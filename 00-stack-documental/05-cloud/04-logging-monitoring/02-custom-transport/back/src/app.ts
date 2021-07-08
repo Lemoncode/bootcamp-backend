b94bccb954b26f279f929f45c14acc929a6242aa
daniel.sanchez <daniel.sanchez@lemoncode.net>
2021-07-08 09:34:13 +0200

    copy previous example

---
+++ 00-stack-documental/05-cloud/04-logging-monitoring/02-custom-transport/back/src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import path from 'path';
+import { createRestApiServer, connectToDBServer } from 'core/servers';
+import { envConstants } from 'core/constants';
+import { logger } from 'core/logger';
+import {
+  logRequestMiddleware,
+  logErrorRequestMiddleware,
+} from 'common/middlewares';
+import { booksApi } from 'pods/book';
+import { securityApi, authenticationMiddleware } from 'pods/security';
+
+const restApiServer = createRestApiServer();
+
+const staticFilesPath = path.resolve(__dirname, envConstants.STATIC_FILES_PATH);
+restApiServer.use('/', express.static(staticFilesPath));
+
+restApiServer.use(logRequestMiddleware(logger));
+
+restApiServer.use('/api/security', securityApi);
+restApiServer.use('/api/books', authenticationMiddleware, booksApi);
+
+restApiServer.use(logErrorRequestMiddleware(logger));
+
+restApiServer.listen(envConstants.PORT, async () => {
+  if (!envConstants.isApiMock) {
+    await connectToDBServer(envConstants.MONGODB_URI);
+    logger.info('Connected to DB');
+  } else {
+    logger.info('Running API mock');
+  }
+  logger.info(`Server ready at port ${envConstants.PORT}`);
+});
