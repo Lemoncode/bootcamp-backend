d72293a896d9c7a7c6aaa9f5bfc4a1485a3d917a
daniel.sanchez <daniel.sanchez@lemoncode.net>
2022-12-02 11:45:17 +0100

    update 02-post

--- 00-stack-documental/04-rest-api/02-rest-api-basics/02-post/index.mjs
+++ 00-stack-documental/04-rest-api/02-rest-api-basics/02-post/index.mjs
@@ -1,39 +1,39 @@
-const http = require('http');
-const { getBookList, getBook, insertBook } = require('./mock-db');
+import http from "http";
+import { getBookList, getBook, insertBook } from "./mock-db.mjs";
 
 const handleRequest = (req, res) => {
   const { url, method } = req;
-  if (url === '/api/books') {
-    if (method === 'GET') {
-      res.setHeader('Content-Type', 'application/json');
-      res.statusCode = 200;
+  if (url === "/api/books") {
+    if (method === "GET") {
+      res.setHeader("Content-Type", "application/json");
       res.write(JSON.stringify(getBookList()));
+      res.statusCode = 200;
       res.end();
-    } else if (method === 'POST') {
+    } else if (method === "POST") {
       let data = [];
-      req.on('data', (chunk) => {
+      req.on("data", (chunk) => {
         console.log({ chunk });
         data += chunk;
       });
 
-      req.on('end', () => {
-        const book = JSON.parse(data.toString('utf8'));
+      req.on("end", () => {
+        const book = JSON.parse(data.toString("utf8"));
         const newBook = insertBook(book);
-        res.setHeader('Content-Type', 'application/json');
+        res.setHeader("Content-Type", "application/json");
         res.statusCode = 201;
         res.write(JSON.stringify(newBook));
         res.end();
       });
     }
-  } else if (/\/api\/books\/\d$/.test(url) && method === 'GET') {
+  } else if (/\/api\/books\/\d$/.test(url) && method === "GET") {
     const [, bookId] = url.match(/\/api\/books\/(\d)$/);
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.statusCode = 200;
     const book = getBook(Number(bookId));
     res.write(JSON.stringify(book));
     res.end();
   } else {
-    res.write('My awesome books portal');
+    res.write("My awesome books portal");
     res.end();
   }
 };
