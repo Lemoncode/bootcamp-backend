af6ecbc70799c91c5b4db1e6143b5aa0439cc469
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-03-01 13:44:15 +0100

    update boilerplate

--- 00-stack-documental/99-beta-nodejs-18/05-cloud/01-storage/00-boilerplate/front/src/pods/user/user.mappers.ts
+++ 00-stack-documental/99-beta-nodejs-18/05-cloud/01-storage/00-boilerplate/front/src/pods/user/user.mappers.ts
@@ -5,7 +5,7 @@ import * as viewModel from './user.vm';
 export const mapUserFromApiToVm = (user: apiModel.User): viewModel.User => ({
   email: user.email,
   role: user.role,
-  avatar: Boolean(envConstants.IMAGES_BASE_URL)
+  avatar: Boolean(envConstants.IMAGES_BASE_URL) && !user.avatar.includes("http")
     ? `${envConstants.IMAGES_BASE_URL}/${user.avatar}`
     : user.avatar,
 });
