5cd1cfa1ff8ca369246d2bb83fda92195144622f
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-03-03 09:39:23 +0100

    refactor example

--- 00-stack-documental/99-beta-nodejs-18/05-cloud/02-deploy/04-manual-aws-deploy/front/src/common/components/table/components/row.component.styles.ts
+++
@@ -1,8 +0,0 @@
-import { css } from '@emotion/css';
-import { theme } from 'core/theme';
-
-export const root = css`
-  &:nth-child(odd) {
-    background-color: ${theme.palette.background.default};
-  }
-`;
