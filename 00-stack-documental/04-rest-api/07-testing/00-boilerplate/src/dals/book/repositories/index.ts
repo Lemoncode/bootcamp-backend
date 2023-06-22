e805fbbc20da4d1eb180eeada4d34f40af7c0efb
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-06-22 09:27:22 +0200

    update 00

--- 00-stack-documental/04-rest-api/07-testing/00-boilerplate/src/dals/book/repositories/index.ts
+++ 00-stack-documental/04-rest-api/07-testing/00-boilerplate/src/dals/book/repositories/index.ts
@@ -1,6 +1,6 @@
-import { mockRepository } from './book.mock-repository';
-import { dbRepository } from './book.db-repository';
-import { envConstants } from 'core/constants';
+import { mockRepository } from "./book.mock-repository.js";
+import { dbRepository } from "./book.db-repository.js";
+import { envConstants } from "#core/constants/index.js";
 
 export const bookRepository = envConstants.isApiMock
   ? mockRepository
--- 00-stack-documental/99-beta-nodejs-18/04-rest-api/07-testing/00-boilerplate/src/dals/user/repositories/user.repository.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../user.model.js';
-
-export interface UserRepository {
-  getUserByEmailAndPassword: (email: string, password: string) => Promise<User>;
-}
