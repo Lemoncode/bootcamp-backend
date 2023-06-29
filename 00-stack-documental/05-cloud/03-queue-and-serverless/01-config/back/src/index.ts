b031f0af9bbe9f1047224a2e67048138a25e9c09
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-06-29 12:27:15 +0200

    update 01

--- 00-stack-documental/05-cloud/03-queue-and-serverless/01-config/back/src/index.ts
+++ 00-stack-documental/05-cloud/03-queue-and-serverless/01-config/back/src/index.ts
@@ -1,4 +1,46 @@
-import { config } from 'dotenv';
-config();
+import '#core/load-env.js';
+import express from 'express';
+import path from 'path';
+import url from 'url';
+import {
+  logRequestMiddleware,
+  logErrorRequestMiddleware,
+} from '#common/middlewares/index.js';
+import {
+  createRestApiServer,
+  connectToDBServer,
+  connectToMessageBrokerServer,
+  messageBroker,
+} from '#core/servers/index.js';
+import { envConstants } from '#core/constants/index.js';
+import { booksApi } from '#pods/book/index.js';
+import { securityApi, authenticationMiddleware } from '#pods/security/index.js';
+import { userApi } from '#pods/user/index.js';
 
-require('./app');
+const restApiServer = createRestApiServer();
+
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const staticFilesPath = path.resolve(__dirname, envConstants.STATIC_FILES_PATH);
+restApiServer.use('/', express.static(staticFilesPath));
+
+restApiServer.use(logRequestMiddleware);
+
+restApiServer.use('/api/security', securityApi);
+restApiServer.use('/api/books', authenticationMiddleware, booksApi);
+restApiServer.use('/api/users', authenticationMiddleware, userApi);
+
+restApiServer.use(logErrorRequestMiddleware);
+
+restApiServer.listen(envConstants.PORT, async () => {
+  if (!envConstants.isApiMock) {
+    await connectToDBServer(envConstants.MONGODB_URI);
+    console.log('Connected to DB');
+  } else {
+    console.log('Running API mock');
+  }
+  await connectToMessageBrokerServer(envConstants.RABBITMQ_URI);
+  const channel = await messageBroker.channel();
+  const queue = await channel.queue('hello-queue', { durable: false });
+  await queue.publish('Hello Rabbit!');
+  console.log(`Server ready at port ${envConstants.PORT}`);
+});
--- 00-stack-documental/99-beta-nodejs-18/05-cloud/03-queue-and-serverless/01-config/back/src/core/servers/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './rest-api.server.js';
-export * from './db.server.js';
-export * from './message-broker.server.js';
