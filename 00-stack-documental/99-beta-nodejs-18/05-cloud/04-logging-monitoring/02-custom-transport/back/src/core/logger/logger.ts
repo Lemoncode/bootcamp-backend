eee2a448ec48c9be69b81e4ba4e1c1da43b0f7a0
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-06-30 14:57:33 +0200

    remove

--- 00-stack-documental/99-beta-nodejs-18/05-cloud/04-logging-monitoring/02-custom-transport/back/src/core/logger/logger.ts
+++
@@ -1,6 +0,0 @@
-import { createLogger } from 'winston';
-import { console, file, rollbar } from './transports';
-
-export const logger = createLogger({
-  transports: [console, file, rollbar],
-});
--- 00-stack-documental/99-beta-nodejs-18/05-cloud/04-logging-monitoring/02-custom-transport/front/src/common/components/table/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './table.container';
-export * from './components/row.component';
-export * from './components/cell.component';
-export * from './table.vm';
