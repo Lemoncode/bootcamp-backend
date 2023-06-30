4d7f22c8284f618b48cbc74a91d1c75f8d1c7d4e
daniel.sanchez <daniel.sanchez@lemoncode.net>
2023-06-30 14:16:57 +0200

    update 00

--- 00-stack-documental/99-beta-nodejs-18/05-cloud/04-logging-monitoring/00-boilerplate/front/src/pods/user/api/user.api.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-import { User } from './user.api-model';
-
-export const getUser = async (): Promise<User> => {
-  const { data } = await axios.get<User>('/api/users');
-  return data;
-};
