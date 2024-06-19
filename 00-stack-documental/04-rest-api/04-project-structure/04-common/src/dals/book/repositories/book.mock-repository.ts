972e408256a504c3ba8148517664b836ab541b38
daniel.sanchez <daniel.sanchez@lemoncode.net>
2024-06-19 19:59:29 +0200

    update 04-common

--- 00-stack-documental/04-rest-api/04-project-structure/04-common/src/dals/book/repositories/book.mock-repository.ts
+++ 00-stack-documental/04-rest-api/04-project-structure/04-common/src/dals/book/repositories/book.mock-repository.ts
@@ -38,9 +38,12 @@ export const mockRepository: BookRepository = {
     paginateBookList(db.books, page, pageSize),
   getBook: async (id: string) => db.books.find((b) => b.id === id),
   saveBook: async (book: Book) =>
-    Boolean(book.id) ? updateBook(book) : insertBook(book),
+    db.books.some((b) => b.id === book.id)
+      ? updateBook(book)
+      : insertBook(book),
   deleteBook: async (id: string) => {
+    const exists = db.books.some((b) => b.id === id);
     db.books = db.books.filter((b) => b.id !== id);
-    return true;
+    return exists;
   },
 };
