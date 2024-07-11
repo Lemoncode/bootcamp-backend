e89577bc9e1507bbb32e8afae8944612f7724255
daniel.sanchez <daniel.sanchez@lemoncode.net>
2024-07-11 18:24:14 +0200

    update 07

---
+++ 00-stack-documental/04-rest-api/07-testing/07-ci/src/core/security/security.middlewares.ts
@@ -0,0 +1,32 @@
+import { RequestHandler, Request, Response, NextFunction } from 'express';
+import { verifyJWT } from '#common/helpers/index.js';
+import { ENV } from '#core/constants/index.js';
+import { UserSession, Role } from '#core/models/index.js';
+
+export const authenticationMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const [, token] = req.cookies.authorization?.split(' ') || [];
+    const userSession = await verifyJWT<UserSession>(token, ENV.AUTH_SECRET);
+    req.userSession = userSession;
+    next();
+  } catch (error) {
+    res.sendStatus(401);
+  }
+};
+
+const isAuthorized = (currentRole: Role, allowedRoles: Role[]) =>
+  Boolean(currentRole) && allowedRoles?.some((role) => currentRole === role);
+
+export const authorizationMiddleware =
+  (allowedRoles: Role[]): RequestHandler =>
+  async (req, res, next) => {
+    if (isAuthorized(req.userSession?.role, allowedRoles)) {
+      next();
+    } else {
+      res.sendStatus(403);
+    }
+  };
