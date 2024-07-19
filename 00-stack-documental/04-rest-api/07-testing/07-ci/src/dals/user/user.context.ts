9e62fcc26a3db9d2f65a7117ea49e9ecad2ccb47
daniel.sanchez <daniel.sanchez@lemoncode.net>
2024-07-19 18:19:36 +0200

    update 07

--- 00-stack-documental/04-rest-api/07-testing/07-ci/src/dals/user/user.context.ts
+++ 00-stack-documental/04-rest-api/07-testing/07-ci/src/dals/user/user.context.ts
@@ -1,4 +1,4 @@
 import { dbServer } from '#core/servers/index.js';
 import { User } from './user.model.js';
 
-export const getUserContext = () => dbServer.db?.collection<User>('users');
+export const getUserContext = () => dbServer?.db.collection<User>('users');
