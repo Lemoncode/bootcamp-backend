cde087c590eebcd6e0865d76f208b90c087239bd
daniel.sanchez <daniel.sanchez@lemoncode.net>
2024-12-05 12:33:24 +0100

    remove serverless demo

--- 00-stack-documental/05-cloud/03-queues/06-serverless/back/src/simple-api.ts
+++
@@ -1,8 +0,0 @@
-export const handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from function',
-    }),
-  };
-};
--- 00-stack-documental/05-cloud/03-queues/06-serverless/consumers/src/core/constants/env.constants.ts
+++
@@ -1,4 +0,0 @@
-export const envConstants = {
-  isProduction: process.env.NODE_ENV === 'production',
-  RABBITMQ_URL: process.env.RABBITMQ_URL,
-};
